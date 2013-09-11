6978661ee9f4b6a83063d95370e7142f2bc7bd65
carlcook <carl.cook@gmail.com>
2013-09-11 17:06:44 +0100

    Initial commit

---
+++ main.cc
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+#include <iostream>
+
+#include "skiplist.h"
+
+using namespace std;
+using namespace nonstd;
+
+class MyDataItem
+{
+  public:
+    MyDataItem()
+    {
+      mData = new int[10];
+      printf("Got created\n");
+    }
+
+    MyDataItem(MyDataItem&& other)
+    {
+      mData = other.mData;
+      id = other.id;
+      other.mData = NULL;
+      printf("Got moved\n");
+    }
+
+    MyDataItem(const MyDataItem& other)
+    {
+      mData = new int(10);
+      id = other.id;
+      printf("Got copy constructed\n");
+    }
+
+    MyDataItem& operator=(const MyDataItem& other)
+    {
+      if (this != &other)
+      {
+        int* newData = new int[10];
+        std::copy(other.mData, other.mData + 10, newData);
+
+        delete [] mData;
+
+        mData = newData;
+        id = other.id;
+        printf("Got assigned\n");
+      }
+      return *this;
+    }
+
+    MyDataItem& operator=(MyDataItem&& other)
+    {
+      if (this != &other)
+      {
+        delete [] mData;
+        mData = other.mData;
+        id = other.id;
+        other.mData = NULL;
+        printf("Got move assigned\n");
+      }
+      return *this;
+    }
+
+    ~MyDataItem()
+    {
+      delete mData;
+      mData = NULL;
+    }
+
+    bool operator< (const MyDataItem& other) const
+    {
+      printf("comparing %d and %d\n", id, other.id);
+      return id < other.id;
+    }
+
+    int id;
+
+  private:
+ int* mData;
+};
+
+int main()
+{
+  skiplist<MyDataItem> myList;
+  MyDataItem item1; item1.id = 1;
+  MyDataItem item2; item2.id = 2;
+  MyDataItem item3; item3.id = 3;
+  myList.insert(item1);
+  myList.insert(item3);
+  myList.insert(item2);
+  auto it = myList.find(item2);
+
+  cout << (it == myList.end()) << endl;
+  cout << it->id << endl;
+  cout << (*it).id << endl;
+
+  for (auto it2 = myList.begin(); it2 != myList.end(); ++it2)
+  {
+    cout << it2->id << endl;
+  }
+
+  cout << "next should be three after deleting two: " << myList.erase(it)->id << endl;
+
+  for (auto it2 = myList.begin(); it2 != myList.end(); ++it2)
+  {
+    cout << it2->id << endl;
+  }
+
+  cout << "size is now: " << myList.size() << endl;
+
+  cout << "last element is now: " << myList[myList.size() - 1].id << endl;
+
+  auto allocator = myList.get_allocator();
+
+  skiplist<MyDataItem> myList2(myList);
+
+  cout << "last element in copy is now: " << myList2[myList2.size() - 1].id << endl;
+}
