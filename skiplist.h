e1e5c2029c2f4d9ef563fa958c48f7f633e3b65e
Carl Cook <carl.cook@gmail.com>
2014-01-04 19:05:48 +0000

    dasfas

--- skiplist.h
+++ skiplist.h
@@ -70,7 +70,6 @@ class skiplist {
     typedef typename allocator_type::value_type value_type;
     typedef typename allocator_type::const_reference const_reference;
     typedef typename allocator_type::const_pointer const_pointer;
-
     typedef typename allocator_type::reference reference;
     typedef typename allocator_type::pointer pointer;
     typedef typename allocator_type::difference_type difference_type;
--- skiplist/README.md
+++
@@ -1,6 +0,0 @@
-cpp-skiplist
-============
-
-stl compliant skip list (sorted list with logarithmic insert/find/erase)
-
-this is just the initial cut - it's not complete, hasn't had tests added, but does the basics
